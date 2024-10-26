58f0e6921de79bd7f20269bd658b9dcd7b7e3acd
Alex <shostak-aleshka@mail.ru>
2024-10-27 01:40:02 +0300

    remove useless routes

--- routes/typeRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const typeController = require("../controllers/typeController");
-const checkRole = require("../middleware/checkPoleMiddleware");
-
-router.get("/", checkRole("ADMIN"), typeController.getAll);
-router.post("/", typeController.create);
-
-module.exports = router;
